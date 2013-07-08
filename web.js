bf616b6e0aaff93b1c4d859d86ac3217eec23b0f
Ubuntu <ubuntu@ip-172-31-31-49.us-west-2.compute.internal>
2013-07-08 06:51:25 +0000

    Added binary data read.

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var binaryIndex = fs.readFileSync("index.html").toString("binary");
+
+var buf = new Buffer(binaryIndex.length);
+buf.write(binaryIndex);
+// console.log(buf.toString());
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
